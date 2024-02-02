b29d22a7c0e6b4516ecc149175fc562b1c172cdc
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-02-02 03:58:44 -0800

    Add files via upload

---
+++ set.py
@@ -0,0 +1,53 @@
+# student_id = {10,12,35,35}
+
+# print("student_id",student_id)
+
+# vowel_latter = {'a','e','i','o','u'}
+# print("vowel_latter:",vowel_latter)
+
+# mixed_data ={'a','d',100}
+
+# print("mixed_data",mixed_data)
+
+# number = {2,3,4,5566,6}
+# print(number)
+
+
+# number = {2,3,4,5566,6}
+# print("initial set",number)
+# number.add(32)
+# print("updated set ",number)
+
+
+# lamguage = {"jello","java" ,"c","ruby"}
+# print("initial set",lamguage)
+# removedValue= lamguage.discard('java')
+# print("updated set",lamguage)
+
+# a = {1,3,5}
+# b = {2,4,6}
+# print("union  union  | ", a | b)
+
+
+
+# a  ={1,3,5}
+# b  = {1,2,3}
+# print('Intersection using &:', a & b)
+
+
+
+
+a  ={1,3,5}
+b  = {1,5,3}
+if  a==b:
+    print('a and b  eqaul')
+else:
+    print('a and b not   eqaul')
+
+
+
+
+
+
+
+
